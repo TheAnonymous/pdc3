e58fa2ee2d2a283359ee0b76698f14c302a03571
Gogs <gogitservice@gmail.com>
2014-12-12 15:31:35 +0100

    changed projectfile

--- WordCount/WordCount.pro
+++ WordCount/WordCount.pro
@@ -4,6 +4,10 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
+#CONFIG += c++11 -fopenmp
+QMAKE_LFLAGS += -fopenmp
+QMAKE_CXXFLAGS += -fopenmp -openmp -std=c++11
+#LIBS += -fopenmp
 
 include(deployment.pri)
 qtcAddDeployment()
--- WordCount/main.cpp
+++ WordCount/main.cpp
@@ -21,60 +21,59 @@ struct word_info{
 
 int main(int argc, char *argv[])
 {
-    int numThreads = 0;
-    int real_numThreads=0;
-    assert(argc == 2);
-    chrono::system_clock::time_point startTime = chrono::system_clock::now();
+	int numThreads = 0;
+	int real_numThreads=0;
+	assert(argc == 2);
+	chrono::system_clock::time_point startTime = chrono::system_clock::now();
 
-    string filename( argv[3], argv[3]+1);
-    int threadnum = atoi(argv[2]);
+	string filename( argv[3], argv[3]+1);
+	int threadnum = atoi(argv[2]);
 
-    assert( 0 < threadnum );
+	assert( 0 < threadnum );
 
-    int linenumber =0;
-    string line;
-    map<string, word_info> wc;
-    ifstream myfile (filename);
-    if (myfile.is_open())
-    {
-      while ( getline (myfile,line) )
-      {
-          linenumber++;
-        //Put code here
-
-
-
-      }
-      myfile.close();
-    }
-    else{
-        cout << "Unable to open file";
-        return 0;
-    }
+	int linenumber =0;
+	string line;
+	map<string, word_info> wc;
+	ifstream myfile (filename);
+	if (myfile.is_open())
+	{
+	  while ( getline (myfile,line) )
+	  {
+		  linenumber++;
+		//Put code here
 
 
-    omp_set_num_threads( numThreads );
 
-    int i, linenumbers;
-    /* Parsing part with openmp */
+	  }
+	  myfile.close();
+	}
+	else{
+		cout << "Unable to open file";
+		return 0;
+	}
 
-    #pragma omp parallel for                            \
-        private(i) lastprivate(real_numThreads)
-    for(int i=0; i < linenumbers; i++){
-        /* do parsing stuff*/
-    }
+	omp_set_num_threads(numThreads);
+
+	int i, linenumbers;
+	/* Parsing part with openmp */
+
+	#pragma omp parallel for                            \
+		private(i) lastprivate(real_numThreads)
+	for(int i=0; i < linenumbers; i++){
+		/* do parsing stuff*/
+	}
 
 
-    chrono::system_clock::time_point endTime = chrono::system_clock::now();
-    chrono::microseconds microRunTime
-         = chrono::duration_cast<chrono::microseconds>(endTime - startTime);
-    double runTime = microRunTime.count() / 1000000.0;
+	chrono::system_clock::time_point endTime = chrono::system_clock::now();
+	chrono::microseconds microRunTime
+		 = chrono::duration_cast<chrono::microseconds>(endTime - startTime);
+	double runTime = microRunTime.count() / 1000000.0;
 
-    cout << std::setprecision( 8 )
-              << "time " << runTime  << " seconds."
-              << endl <<flush;
-    cout << "There were " << real_numThreads << " threads." << endl;
+	cout << std::setprecision( 8 )
+			  << "time " << runTime  << " seconds."
+			  << endl <<flush;
+	cout << "There were " << real_numThreads << " threads." << endl;
 
-    return 0;
+	return 0;
 }
 
