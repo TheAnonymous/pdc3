8f46d64ccb54812870f4acc47ff045f337722393
Gogs <gogitservice@gmail.com>
2014-12-12 15:01:23 +0100

    added code

--- WordCount/main.cpp
+++ WordCount/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main()
+struct word_info{
+	int count;
+	vector<int> lines;
+} word;
+
+int main(int argc, char *argv[])
 {
-	cout << "Hello World!" << endl;
+	string filename( argv[3], argv[3]+1);
+	int threadnum = atoi(argv[2]);
+	int linenumber =0;
+	string line;
+	map<string, word_info> wc;
+	ifstream myfile (filename);
+	if (myfile.is_open())
+	{
+	  while ( getline (myfile,line) )
+	  {
+		  linenumber++;
+		//Put code here
+
+
+
+	  }
+	  myfile.close();
+	}
+
+	else cout << "Unable to open file";
 	return 0;
 }
 
